484ed986ac9779a752fd2e7485c8af02cf2606c1
Jason VenHuizen <jason@sharptree.io>
2025-07-18 09:29:30 -0700

    Add support for .jy files.

--- src/maximo/maximo-client.js
+++ src/maximo/maximo-client.js
@@ -782,7 +782,7 @@ export default class MaximoClient {
             await this.connect();
         }
 
-        let isPython = fileName.endsWith('.py');
+        let isPython = fileName.endsWith('.py') || fileName.endsWith('.jy');
 
         progress.report({
             increment: 10,
